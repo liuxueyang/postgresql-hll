1bd04f9cac5c03b74e59902ee6a2aba8b139c7cb
liuxueyang <liuxueyang457@gmail.com>
2022-08-15 11:42:59 +0800

    add pipeline

---
+++ concourse/scripts/build_postgresql-hll.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+
+set -exo pipefail
+
+CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TOP_DIR=${CWDIR}/../../../
+
+source "${TOP_DIR}/gpdb_src/concourse/scripts/common.bash"
+function pkg() {
+    [ -f /opt/gcc_env.sh ] && source /opt/gcc_env.sh
+    source /usr/local/greenplum-db-devel/greenplum_path.sh
+
+    if [ "${OS_NAME}" = "rhel6" ] || [ "${OS_NAME}" = "rhel7" ]; then
+        export CC="$(which gcc)"
+    fi
+
+    pushd /home/gpadmin/postgresql-hll_src
+    make clean
+    make
+    make install
+    popd
+
+    mkdir -p $TOP_DIR/bin_postgresql-hll
+
+    CURRENT_VERSION=$(cat /home/gpadmin/postgresql-hll_src/VERSION)
+    $CWDIR/pack.sh -p /usr/local/greenplum-db-devel/ -f "$TOP_DIR/bin_postgresql-hll/postgresql-hll-${CURRENT_VERSION}-${OS_NAME}_x86_64.tar.gz"
+    cp "$TOP_DIR/bin_postgresql-hll/postgresql-hll-${CURRENT_VERSION}-${OS_NAME}_x86_64.tar.gz" "$TOP_DIR/bin_postgresql-hll/postgresql-hll.tar.gz"
+}
+
+function _main() {
+    time pkg
+}
+
+_main "$@"
