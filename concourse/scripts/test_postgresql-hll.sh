4091dae0ba05bd73906581f65b873d72c1d117f8
liuxueyang <liuxueyang457@gmail.com>
2022-08-18 11:30:14 +0800

    test install_gpdb_component script in test_postgresql-hll.sh

--- concourse/scripts/test_postgresql-hll.sh
+++ concourse/scripts/test_postgresql-hll.sh
@@ -3,7 +3,11 @@
 set -exo pipefail
 
 function _main() {
-    tar -xzf /home/gpadmin/bin_postgresql-hll/postgresql-hll-*_x86_64.tar.gz -C /usr/local/greenplum-db-devel
+    unpack_dir=/home/gpadmin/bin_postgresql-hll/postgresql-hll_unpack
+    mkdir -p ${unpack_dir}
+    tar -xzf /home/gpadmin/bin_postgresql-hll/postgresql-hll-*_x86_64.tar.gz -C ${unpack_dir}
+    ${unpack_dir}/install_gpdb_component
+
     source /home/gpadmin/gpdb_src/gpAux/gpdemo/gpdemo-env.sh
 
     pushd /home/gpadmin/postgresql-hll_src
