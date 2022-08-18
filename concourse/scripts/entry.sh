9087fad5105433230179af8dd5ffa773f1d1488e
liuxueyang <liuxueyang457@gmail.com>
2022-08-18 16:38:47 +0800

    remove redundant comment

--- concourse/scripts/entry.sh
+++ concourse/scripts/entry.sh
@@ -116,9 +116,6 @@ setup_gpadmin_bashrc
 # gpadmin. bashrc won't be read by 'su', it needs to be sourced explicitly.
 case "$1" in
     build)
-        # There are some different behaviors between ubuntu and rhel. On ubuntu, build_postgresql-hll.sh
-        # can't get the exported variable OS_NAME from .bashrc. I set it in the command passed
-        # to `su` as a workaround.
         su gpadmin -c \
            "source /home/gpadmin/.bashrc && \
             /home/gpadmin/postgresql-hll_src/concourse/scripts/build_postgresql-hll.sh"
